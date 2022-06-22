d7c019e0f802883e533c2967a12944717d385784
IgorLockyh <igorylickh@gmail.com>
2022-06-22 19:12:27 +0300

    add cleanupdemouser as a command

---
+++ app/Console/Commands/CleanUpDemoUsers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\DemoService;
+
+class CleanUpDemoUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'demo:cleanup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean up demo users data';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(DemoService $demoService)
+    {
+        $demoService->cleanUpDemoUserData();
+    }
+}

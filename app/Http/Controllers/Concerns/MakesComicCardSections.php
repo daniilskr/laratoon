f5616f7de1a757355b97dcc7de989eb9c716869b
IgorLockyh <igorylickh@gmail.com>
2023-02-03 19:46:58 +0300

    Some more refactoring

---
+++ app/Http/Controllers/Concerns/MakesComicCardSections.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Concerns;
+
+use App\Http\Resources\ComicCardResource;
+use App\Http\Resources\ImageResource;
+use Illuminate\Database\Eloquent\Builder;
+
+trait MakesComicCardSections
+{
+    protected function makeSection(Builder $query, int $limit, string $title)
+    {
+        return [
+            'title' => $title,
+            'comicCards' => ComicCardResource::collection($comics = $query->limit($limit)->get()),
+            'sectionPoster' => new ImageResource($comics->first()->comicHeaderBackground->image),
+        ];
+    }
+
+    protected function makeSmallWideCardsSection(Builder $query, string $title)
+    {
+        return array_merge($this->makeSection($query, 6, $title), [
+            'type' => 'small_wide_cards',
+        ]);
+    }
+
+    protected function makeGoToMoreSection(Builder $query, string $title)
+    {
+        return array_merge($this->makeSection($query, 4, $title), [
+            'type' => 'go_to_more',
+        ]);
+    }
+}
--- app/Http/Controllers/HomeComicCardsSectionsController.php
+++ app/Http/Controllers/HomeComicCardsSectionsController.php
@@ -2,35 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ComicCardResource;
-use App\Http\Resources\ImageResource;
+use App\Http\Controllers\Concerns\MakesComicCardSections;
 use App\Models\Comic;
-use Illuminate\Database\Eloquent\Builder;
 
 class HomeComicCardsSectionsController extends Controller
 {
-    protected function makeSection(Builder $query, int $limit, string $title)
-    {
-        return [
-            'title' => $title,
-            'comicCards' => ComicCardResource::collection($comics = $query->limit($limit)->get()),
-            'sectionPoster' => new ImageResource($comics->first()->comicHeaderBackground->image),
-        ];
-    }
-
-    protected function makeSmallWideCardsSection(Builder $query, string $title)
-    {
-        return array_merge($this->makeSection($query, 6, $title), [
-            'type' => 'small_wide_cards',
-        ]);
-    }
-
-    protected function makeGoToMoreSection(Builder $query, string $title)
-    {
-        return array_merge($this->makeSection($query, 4, $title), [
-            'type' => 'go_to_more',
-        ]);
-    }
+    use MakesComicCardSections;
 
     public function __invoke()
     {

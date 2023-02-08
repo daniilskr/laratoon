78810f4c5750e9fd2fe5ffb0cf9701503fba9886
IgorLockyh <igorylickh@gmail.com>
2023-02-08 11:12:16 +0300

    remove useless docblocks in controllers

--- app/Http/Controllers/VueApplicationController.php
+++ app/Http/Controllers/VueApplicationController.php
@@ -4,11 +4,6 @@ namespace App\Http\Controllers;
 
 class VueApplicationController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke()
     {
         return view('vue.app');

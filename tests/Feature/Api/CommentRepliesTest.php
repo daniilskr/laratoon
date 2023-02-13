f58ac8b3a7732fbc73fa0d645a9f69f057ff3c6c
IgorLockyh <igorylickh@gmail.com>
2023-02-14 01:50:18 +0300

    add replies to replies tests

--- tests/Feature/Api/CommentRepliesTest.php
+++ tests/Feature/Api/CommentRepliesTest.php
@@ -20,13 +20,8 @@ class CommentRepliesTest extends TestCase
 
     public function test_can_post_a_comment_reply(): void
     {
-        $comment = Comment::factory()
-                        ->for($this->createUser())
-                        ->for(Commentable::factory()->create())
-                        ->create();
-
         $response = $this->postCommentReply(
-            $comment,
+            $this->createRootComment(),
             $this->createUser(),
             $this->randomCommentText(),
         );
@@ -34,15 +29,23 @@ class CommentRepliesTest extends TestCase
         $response->assertStatus(201);
     }
 
+    public function test_can_post_reply_to_a_reply()
+    {
+        $response = $this->postCommentReply(
+            $this->postParentReply(),
+            $this->createUser(),
+            $this->randomCommentText(),
+        );
+
+        $response
+            ->assertStatus(201)
+            ->assertJsonPath('data.id', fn ($id) => is_int($id));
+    }
+
     public function test_posted_reply_appears_in_the_database(): void
     {
-        $comment = Comment::factory()
-            ->for($this->createUser())
-            ->for(Commentable::factory()->create())
-            ->create();
-
         $response = $this->postCommentReply(
-            $comment,
+            $comment = $this->createRootComment(),
             $user        = $this->createUser(),
             $commentText = $this->randomCommentText(),
         );
@@ -60,15 +63,31 @@ class CommentRepliesTest extends TestCase
         ]);
     }
 
+    public function test_posted_reply_to_a_reply_appears_in_database()
+    {
+        $parentReply = $this->postParentReply(
+            $rootComment = $this->createRootComment()
+        );
+
+        $replyToReplyId = $this->postCommentReply(
+            $parentReply,
+            $user        = $this->createUser(),
+            $commentText = $this->randomCommentText(),
+        )->json('data.id');
+
+        $this->assertDatabaseHas(Comment::class, [
+            'comment_text' => $commentText,
+            'user_id' => $user->id,
+            'root_comment_id' => $rootComment->id,
+            'parent_comment_id' => $parentReply->id,
+            'id' => $replyToReplyId,
+        ]);
+    }
+
     public function test_can_see_posted_reply_in_comment_replies_with_root_resource(): void
     {
-        $comment = Comment::factory()
-            ->for($this->createUser())
-            ->for(Commentable::factory()->create())
-            ->create();
-
         $response = $this->postCommentReply(
-            $comment,
+            $comment = $this->createRootComment(),
             $this->createUser(),
             $this->randomCommentText(),
         );
@@ -89,12 +108,42 @@ class CommentRepliesTest extends TestCase
             );
     }
 
+    public function test_can_see_posted_reply_to_a_reply_in_comment_replies_with_root_resource(): void
+    {
+        $parentReply = $this->postParentReply(
+            $rootComment = $this->createRootComment()
+        );
+
+        $replyToReplyId = $this->postCommentReply(
+            $parentReply,
+            $this->createUser(),
+            $this->randomCommentText(),
+        )->json('data.id');
+
+        $response = $this->get(route('comment_replies_with_root', ['root' => $rootComment->id]));
+
+        $response
+            ->assertJson(
+                fn (AssertableJson $json) => $json
+                    ->has('data', 2)
+                    ->has(
+                        'data.0',
+                        fn (AssertableJson $json) => $json->where('id', $replyToReplyId)->etc()
+                    )
+                    ->has(
+                        'data.1',
+                        fn (AssertableJson $json) => $json->where('id', $parentReply->id)->etc()
+                    )
+                    ->etc()
+            );
+    }
+
     public function test_can_see_posted_reply_in_user_profile(): void
     {
         $comment = Comment::factory()
-            ->for($this->createUser())
-            ->for(Comic::factory()->create()->commentable)
-            ->create();
+                        ->for($this->createUser())
+                        ->for(Comic::factory()->create()->commentable)
+                        ->create();
 
         $response = $this->postCommentReply(
             $comment,
@@ -120,12 +169,9 @@ class CommentRepliesTest extends TestCase
 
     public function test_posted_reply_increments_commentable_comments_cached_count(): void
     {
-        $comment = Comment::factory()
-                        ->for($this->createUser())
-                        ->for($commentable = Commentable::factory()->create())
-                        ->create();
+        $comment = $this->createRootComment();
 
-        $this->assertEquals(1, $commentable->refresh()->comments_cached_count);
+        $this->assertEquals(1, $comment->commentable->refresh()->comments_cached_count);
 
         $this->postCommentReply(
             $comment,
@@ -133,7 +179,41 @@ class CommentRepliesTest extends TestCase
             $this->randomCommentText(),
         );
 
-        $this->assertEquals(2, $commentable->refresh()->comments_cached_count);
+        $this->assertEquals(2, $comment->commentable->refresh()->comments_cached_count);
+    }
+
+    public function test_posted_reply_to_a_reply_increments_commentable_comments_cached_count(): void
+    {
+        $parentReply = $this->postParentReply(
+            $rootComment = $this->createRootComment()
+        );
+
+        $this->assertEquals(2, $rootComment->commentable->refresh()->comments_cached_count);
+
+        $this->postCommentReply(
+            $parentReply,
+            $this->createUser(),
+            $this->randomCommentText(),
+        );
+
+        $this->assertEquals(3, $rootComment->commentable->refresh()->comments_cached_count);
+    }
+
+    protected function createRootComment(): Comment
+    {
+        return Comment::factory()
+            ->for($this->createUser())
+            ->for(Commentable::factory()->create())
+            ->create();
+    }
+
+    protected function postParentReply(?Comment $rootComment = null): Comment
+    {
+        return Comment::find($this->postCommentReply(
+            $rootComment ?? $this->createRootComment(),
+            $this->createUser(),
+            $this->randomCommentText(),
+        )->json('data.id'));
     }
 
     protected function postCommentReply(Comment $comment, User $user, string $commentText): TestResponse

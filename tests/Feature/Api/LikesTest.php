3fd712aa39bd5f09371af50d738019ef45207d9c
IgorLockyh <igorylickh@gmail.com>
2023-02-14 20:22:49 +0300

    add more tests, refactoring

--- tests/Feature/Api/LikesTest.php
+++ tests/Feature/Api/LikesTest.php
@@ -15,6 +15,8 @@ class LikesTest extends TestCase
 
     public function test_can_post_like(): void
     {
+        $this->actingAs($this->createUser());
+
         $response = $this->postLike();
 
         $response->assertStatus(201);
@@ -22,22 +24,17 @@ class LikesTest extends TestCase
 
     public function test_can_delete_like(): void
     {
-        $this->postLike(
-            $user     = $this->createUser(),
-            $likeable = $this->createLikeable(),
-        );
+        $this->actingAs($this->createUser());
+        $this->postLike($likeable = $this->createLikeable());
 
-        $response = $this->deleteLike($user, $likeable);
-
+        $response = $this->deleteLike($likeable);
         $response->assertStatus(200);
     }
 
     public function test_posted_like_appears_in_the_database(): void
     {
-        $this->postLike(
-            $user     = $this->createUser(),
-            $likeable = $this->createLikeable(),
-        );
+        $this->actingAs($user = $this->createUser());
+        $this->postLike($likeable = $this->createLikeable());
 
         $this->assertDatabaseHas(Like::class, [
             'user_id' => $user->id,
@@ -47,12 +44,9 @@ class LikesTest extends TestCase
 
     public function test_deleted_like_is_not_present_in_the_database(): void
     {
-        $this->postLike(
-            $user     = $this->createUser(),
-            $likeable = $this->createLikeable(),
-        );
-
-        $this->deleteLike($user, $likeable);
+        $this->actingAs($user = $this->createUser());
+        $this->postLike($likeable = $this->createLikeable());
+        $this->deleteLike($likeable);
 
         $this->assertDatabaseMissing(Like::class, [
             'user_id' => $user->id,
@@ -60,18 +54,49 @@ class LikesTest extends TestCase
         ]);
     }
 
-    protected function postLike(?User $user = null, ?Likeable $likeable = null): TestResponse
+    public function test_posted_like_increments_likeable_likes_cached_count(): void
     {
-        return $this->actingAs($user ?? $this->createUser())->post(
+        $this->actingAs($this->createUser());
+
+        $this->assertEquals(0, ($likeable = $this->createLikeable())->likes_cached_count);
+
+        $this->postLike($likeable);
+
+        $this->assertEquals(1, $likeable->refresh()->likes_cached_count);
+    }
+
+    public function test_deleted_like_decrements_likeable_likes_cached_count(): void
+    {
+        $this->actingAs($this->createUser());
+        $this->postLike($likeable = $this->createLikeable());
+
+        $this->assertEquals(1, $likeable->refresh()->likes_cached_count);
+
+        $this->deleteLike($likeable);
+
+        $this->assertEquals(0, $likeable->refresh()->likes_cached_count);
+    }
+
+    public function test_can_get_request_user_like_from_model(): void
+    {
+        $this->actingAs($this->createUser());
+        $this->postLike($likeable = $this->createLikeable());
+
+        $this->assertNotNull($likeable->refresh()->requestUserLike);
+    }
+
+    protected function postLike(?Likeable $likeable = null): TestResponse
+    {
+        return $this->post(
             route('likeables.like.store', [
                 'likeable' => $likeable ?? $this->createLikeable(),
             ])
         );
     }
 
-    protected function deleteLike(?User $user = null, ?Likeable $likeable = null): TestResponse
+    protected function deleteLike(?Likeable $likeable = null): TestResponse
     {
-        return $this->actingAs($user ?? $this->createUser())->delete(route('likeables.like.destroy', [
+        return $this->delete(route('likeables.like.destroy', [
             'likeable' => $likeable ?? $this->createLikeable(),
         ]));
     }

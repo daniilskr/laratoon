6d358c59ca4805cadee837d73785a32f1cdc0634
IgorLockyh <igorylickh@gmail.com>
2023-02-21 05:27:15 +0300

    get rid off HasAnonymousFactory

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Image extends Model
 {
-    use Concerns\HasAnonymousFactory;
+    use HasFactory;
 
     protected $fillable = [
         'medium',

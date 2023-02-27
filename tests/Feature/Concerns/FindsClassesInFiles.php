5509da441ca75fb6fbcc787869065fe6ccfab315
IgorLockyh <igorylickh@gmail.com>
2023-02-27 17:47:21 +0300

    phpstan

--- tests/Feature/Concerns/FindsClassesInFiles.php
+++ tests/Feature/Concerns/FindsClassesInFiles.php
@@ -11,16 +11,19 @@ trait FindsClassesInFiles
      * Extract the class name from the given file path.
      *
      * @param  \SplFileInfo  $file
-     * @return string
+     * @return class-string
      */
-    protected static function classFromFile(SplFileInfo $file)
+    protected static function classFromFile(SplFileInfo $file): string
     {
         $class = trim(Str::replaceFirst(base_path(), '', $file->getRealPath()), DIRECTORY_SEPARATOR);
 
-        return str_replace(
+        /** @var class-string */
+        $class = str_replace(
             [DIRECTORY_SEPARATOR, ucfirst(basename(app()->path())).'\\'],
             ['\\', app()->getNamespace()],
             ucfirst(Str::replaceLast('.php', '', $class))
         );
+
+        return $class;
     }
 }

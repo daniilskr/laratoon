d0a8a6202093ad86d5b67721653238a9385b909a
IgorLockyh <igorylickh@gmail.com>
2023-03-14 22:44:06 +0300

    csfix

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -155,7 +155,7 @@ $rules = [
 $finder = Finder::create()
     ->in([
         __DIR__ . '/app',
-        // __DIR__ . '/config',
+        __DIR__ . '/config',
         __DIR__ . '/database',
         __DIR__ . '/resources',
         __DIR__ . '/routes',

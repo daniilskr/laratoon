b4914bc34d2f1a4fde6c70f3211b6d45669f67fd
IgorLockyh <igorylickh@gmail.com>
2023-04-10 11:49:57 +0300

    refactoring

--- app/Http/Controllers/CatalogFiltersController.php
+++ app/Http/Controllers/CatalogFiltersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ComicTagResource;
+use App\Http\Resources\GenreResource;
+use App\Http\Resources\PublicationStatusResource;
 use App\Models\ComicTag;
 use App\Models\Genre;
 use App\Models\PublicationStatus;
@@ -11,21 +14,9 @@ class CatalogFiltersController extends Controller
     public function __invoke()
     {
         return [
-            'genres' => Genre::all()->map(fn ($genre) => [
-                'id' => $genre->id,
-                'name' => $genre->name,
-                'slug' => $genre->slug,
-            ]),
-            'tags' => ComicTag::all()->map(fn ($tag) => [
-                'id' => $tag->id,
-                'name' => $tag->name,
-                'slug' => $tag->slug,
-            ]),
-            'statuses' => PublicationStatus::all()->map(fn ($status) => [
-                'id' => $status->id,
-                'name' => $status->name,
-                'slug' => $status->slug,
-            ]),
+            'genres' => GenreResource::collection(Genre::all()),
+            'tags' => ComicTagResource::collection(ComicTag::all()),
+            'statuses' => PublicationStatusResource::collection(PublicationStatus::all()),
         ];
     }
 }

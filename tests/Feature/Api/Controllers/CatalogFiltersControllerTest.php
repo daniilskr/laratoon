bc0af0ae6f8698238d5bf7a683b22a1a84c05a63
IgorLockyh <igorylickh@gmail.com>
2023-04-11 08:49:23 +0300

    add more controllers tests

---
+++ tests/Feature/Api/Controllers/CatalogFiltersControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Api\Controllers;
+
+use App\Models\Comic;
+use App\Models\ComicTag;
+use App\Models\Genre;
+use App\Models\PublicationStatus;
+use Database\Factories\ComicTagFactory;
+use Database\Factories\GenreFactory;
+use Database\Factories\PublicationStatusFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class CatalogFiltersControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_catalog_filters_match_the_specification(): void
+    {
+        Genre::factory(count(GenreFactory::GENRES))->create();
+        ComicTag::factory(count(ComicTagFactory::TAGS))->create();
+        PublicationStatus::factory(count(PublicationStatusFactory::STATUSES))->create();
+
+        $response = $this->get('/api/catalog-filters');
+
+        $specification = [
+            'genres' => [
+                0 => [
+                    'id' => 'integer',
+                    'name' => 'string',
+                    'slug' => 'string',
+                ],
+            ],
+            'statuses' => [
+                0 => [
+                    'id' => 'integer',
+                    'name' => 'string',
+                    'slug' => 'string',
+                ],
+            ],
+            'tags' => [
+                0 => [
+                    'id' => 'integer',
+                    'name' => 'string',
+                    'slug' => 'string',
+                ],
+            ],
+        ];
+
+        $response
+            ->assertOk()
+            ->assertJson(
+                fn (AssertableJson $json) => $json
+                ->whereAllType(Arr::dot($specification))
+            );
+    }
+}
---
+++ tests/Feature/Api/Controllers/HomeComicCardsSectionsControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Api\Controllers;
+
+use App\Models\Comic;
+use App\Models\Genre;
+use Database\Factories\GenreFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class HomeComicCardsSectionsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_home_comic_cards_sections_match_the_specification(): void
+    {
+        Comic::factory(5)->hasGenresAttached(count(GenreFactory::GENRES))->create();
+
+        $response = $this->get('/api/home-comic-cards-sections');
+
+        $specification = [
+            0 => [
+                'type' => 'string',
+                'title' => 'string',
+                'comicCards' => [
+                    0 => [
+                        'id' => 'integer',
+                        'slug' => 'string',
+                        'title' => 'string',
+                        'description' => 'string',
+                        'author' => [
+                            'id' => 'integer',
+                            'fullName' => 'string',
+                        ],
+                        'statistics' => [
+                            'likes' => [
+                                'total' => 'integer',
+                            ],
+                        ],
+                        'comicPoster' => [
+                            'medium' => 'string',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $response
+            ->assertOk()
+            ->assertJson(
+                fn (AssertableJson $json) => $json
+                ->whereAllType(Arr::dot($specification))
+            );
+    }
+}
